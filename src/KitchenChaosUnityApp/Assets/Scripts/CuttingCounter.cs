608134d2a131c06c029bc85e97f5944e3f17d602
Dmitry Evmenov <DmitryEvmenov@coherentsolutions.com>
2024-03-11 19:48:50 +0300

    Added progress bar for cutting

--- src/KitchenChaosUnityApp/Assets/Scripts/CuttingCounter.cs
+++ src/KitchenChaosUnityApp/Assets/Scripts/CuttingCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,16 @@ using UnityEngine;
 public class CuttingCounter : BaseCounter
 {
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+
+    private int cuttingProgress;
+
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+
+    public event EventHandler OnCut;
 
     public override void Interact(Player player)
     {
@@ -18,6 +29,9 @@ public class CuttingCounter : BaseCounter
             if (player.HasKitchenObject)
             {
                 player.DropKitchenObjectTo(this);
+
+                cuttingProgress = 0;
+                NotifyUpdateCuttingProgress();
             }
         }
     }
@@ -32,20 +46,32 @@ public class CuttingCounter : BaseCounter
                 return;
             }
 
-            var objectToSpawn = GetOutputForInput(currentKitchenObject.KitchenObjectSO);
+            cuttingProgress++;
 
-            currentKitchenObject.DestroySelf();
-            KitchenObject.Spawn(objectToSpawn, kitchenObjectParent: this);
+            var recipeSO = GetCuttingRecipeSO(currentKitchenObject.KitchenObjectSO);
+
+            NotifyUpdateCuttingProgress(recipeSO.cuttingProgressMax);
+            OnCut?.Invoke(this, EventArgs.Empty);
+
+            if (cuttingProgress >= recipeSO.cuttingProgressMax)
+            {
+                currentKitchenObject.DestroySelf();
+                KitchenObject.Spawn(recipeSO.output, kitchenObjectParent: this);
+            }
         }
     }
 
-    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO) => 
-        cuttingRecipeSOArray.First(recipes => recipes.input == inputKitchenObjectSO).output;
+    private void NotifyUpdateCuttingProgress(int? cuttingProgressMax = null) => 
+        OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs { progressNormalized = (float)cuttingProgress / cuttingProgressMax ?? 1 });
 
 
     public override bool CanInteract(Player player) => 
         HasKitchenObject 
             || (player.HasKitchenObject && HasValidRecipeFor(player.GetKitchenObject().KitchenObjectSO));
 
-    private bool HasValidRecipeFor(KitchenObjectSO kitchenObjectSO) => cuttingRecipeSOArray.Any(recipes => recipes.input == kitchenObjectSO);
+    private bool HasValidRecipeFor(KitchenObjectSO kitchenObjectSO) => 
+        cuttingRecipeSOArray.Any(recipes => recipes.input == kitchenObjectSO);
+
+    private CuttingRecipeSO GetCuttingRecipeSO(KitchenObjectSO inputKitchenObjectSO) =>
+        cuttingRecipeSOArray.First(recipes => recipes.input == inputKitchenObjectSO);
 }
